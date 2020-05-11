82466f75155f90a9b14a24fc3ead47cd286918bb
Conor Byrne <cjjb95@gmail.com>
2020-05-11 15:32:45 +0100

    Imported ShadowMultiplayer Game.

---
+++ ShadowMultiplayer/RoboCat/Inc/Engine.h
@@ -0,0 +1,29 @@
+class Engine
+{
+public:
+
+	virtual ~Engine();
+	static std::unique_ptr< Engine >	sInstance;
+
+	virtual int		Run();
+	void			SetShouldKeepRunning( bool inShouldKeepRunning ) { mShouldKeepRunning = inShouldKeepRunning; }
+	virtual void	HandleEvent(sf::Event &p_event);
+	virtual bool	PullEvent(sf::Event &p_event);
+
+protected:
+
+	Engine();
+
+	virtual void	DoFrame();
+
+private:
+
+
+			
+			int		DoRunLoop();
+
+			bool	mShouldKeepRunning;
+
+
+
+};
