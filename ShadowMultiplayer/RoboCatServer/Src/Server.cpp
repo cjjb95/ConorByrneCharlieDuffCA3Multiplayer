a3fb15a8c19c750cb1266661588a3d7e78817138
Charlie Duff <d00183790@student.dkit.ie>
2020-05-12 15:46:20 +0100

    Removed Additional Spawn Points
    Removed additional spawn points for testing purposes

--- ShadowMultiplayer/RoboCatServer/Src/Server.cpp
+++ ShadowMultiplayer/RoboCatServer/Src/Server.cpp
@@ -32,17 +32,17 @@ Server::Server()
 	//NetworkManagerServer::sInstance->SetSimulatedLatency( 0.1f );
 
 	m_spawnPoints = {
-		Vector3(128, 1088, 0),
+		//Vector3(128, 1088, 0),
 		Vector3(64, 448, 0),
-		Vector3(960, 1088, 0),
-		Vector3(1216, 1088, 0),
-		Vector3(768, 64, 0),
-		Vector3(1728, 64, 0),
-		Vector3(1728, 384, 0),
-		Vector3(1728, 640, 0),
-		Vector3(1728, 1088, 0),
-		Vector3(1152, 1088, 0),
-		Vector3(704, 832, 0)
+		//Vector3(960, 1088, 0),
+		//Vector3(1216, 1088, 0),
+		//Vector3(768, 64, 0),
+		//Vector3(1728, 64, 0),
+		//Vector3(1728, 384, 0),
+		//Vector3(1728, 640, 0),
+		//Vector3(1728, 1088, 0),
+		//Vector3(1152, 1088, 0),
+		//Vector3(704, 832, 0)
 	};
 
 }
